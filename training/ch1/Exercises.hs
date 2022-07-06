8bb75f7cb116ae04cc53df69ca042b49a09d886b
anton2920 <anton2920@gmail.com>
2022-07-06 15:23:01 +0300

    Added dual-pivot five-way qsort implementation

--- training/ch1/Exercises.hs
+++ training/ch1/Exercises.hs
@@ -16,3 +16,15 @@ lqsort (x:xs) = lqsort smaller ++ equal ++ lqsort larger
                     equal = filter (==x) (x:xs)
                     smaller = filter (<x) xs
                     larger = filter (>x) xs
+
+-- Local definition of dual-pivot five-way `qsort`
+lqsort5 :: Ord a => [a] -> [a]
+lqsort5 [] = []
+lqsort5 [x] = [x]
+lqsort5 (x:y:xs) = lqsort5 l1 ++ eq1 ++ lqsort5 g1l2 ++ eq2 ++ lqsort5 g2
+                    where
+                        l1 = filter (< min x y) xs
+                        eq1 = filter (== min x y) (x:y:xs)
+                        g1l2 = filter (\z -> (z > min x y) && (z < max x y)) xs
+                        eq2 = filter (\z -> (z == max x y) && (x /= y)) (x:y:xs)
+                        g2 = filter (> max x y) xs
