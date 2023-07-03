4f56474ca0673b5e4441827ddd3636f8769c4601
anton2920 <anton2920@gmail.com>
2023-07-04 02:58:47 +0300

    Experiments: added proper Applicative instance and three simple parsers

--- experiments/monad/Main.hs
+++ experiments/monad/Main.hs
@@ -1,7 +1,10 @@
 module Main where
 
+import Control.Applicative
+import Data.Char
+
 newtype Parser a = Parser
-	{ runParser :: String -> [(a, String)] }
+	{runParser :: String -> [(a, String)]}
 
 instance Functor Parser where
 	fmap :: (a -> b) -> Parser a -> Parser b
@@ -14,11 +17,11 @@ instance Applicative Parser where
 	pure x = Parser (\xs -> [(x, xs)])
 
 	(<*>) :: Parser (a -> b) -> Parser a -> Parser b
-	(<*>) f pa = Parser (\xs -> case runParser pa xs of
+	(<*>) f pa = Parser (\xs -> case runParser f xs of
 					[]         -> []
-					[(a, xs')] -> case runParser f xs' of
+					[(g, xs')] -> case runParser pa xs' of
 							[]          -> []
-							[(g, xs'')] -> [(g a, xs'')])
+							[(a, xs'')] -> [(g a, xs'')])
 
 instance Monad Parser where
 	(>>=) :: Parser a -> (a -> Parser b) -> Parser b
@@ -26,5 +29,23 @@ instance Monad Parser where
 					[]         -> []
 					[(a, xs')] -> runParser (f a) xs')
 
+instance Alternative Parser where
+	empty :: Parser a
+	empty = Parser (\_ -> [])
+	
+	(<|>) :: Parser a -> Parser a -> Parser a
+	(<|>) f g = Parser (\xs -> case runParser f xs of
+					[]         -> runParser g xs
+					[(a, xs')] -> [(a, xs')])
+
+charP :: Char -> Parser Char
+charP x = Parser (\(y:ys) -> (if x == y then [(y, ys)] else []))
+
+stringP :: String -> Parser String
+stringP xs = mapM charP xs
+
+spaceP :: Parser Char
+spaceP = charP ' ' <|> charP '\t'
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = putStrLn $ show $ runParser (stringP "hello") "hello, world!"
